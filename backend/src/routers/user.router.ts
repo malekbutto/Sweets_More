c635d5e427fc243174d8920ce9e558d57739e6c5
Malek Butto <malekbutto@gmail.com>
2023-02-02 13:13:00 +0200

    make "process.env.JWT_SECRET!" as string

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -71,7 +71,7 @@ const generateTokenResponse = (user: any) => {
       email: user.email,
       isAdmin: user.isAdmin
     },
-      process.env.JWT_SECRET!,
+      "process.env.JWT_SECRET!",
     {
       expiresIn: "30d",
     }
