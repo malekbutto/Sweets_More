e6cd9943af8d30e865134b2af609cfa316b7e136
Malek Butto <malekbutto@gmail.com>
2023-02-06 01:40:22 +0200

    update status

--- backend/src/constants/order_status.ts
+++ backend/src/constants/order_status.ts
@@ -1,7 +1,7 @@
 export enum OrderStatus {
-  NEW = "NEW",
-  PAYED = "PAYED",
-  SHIPPED = "SHIPPED",
-  CANCELED = "CANCELED",
-  REFUNDED = "REFUNDED",
+  NEW = "NEW"
+  // PAYED = "PAYED",
+  // SHIPPED = "SHIPPED",
+  // CANCELED = "CANCELED",
+  // REFUNDED = "REFUNDED",
 }
