5b6481f27b8483d33a36e27adf42423f239d1c22
Malek Butto <malekbutto@gmail.com>
2023-04-17 03:16:26 +0300

    Delete product

--- frontend/src/app/components/partials/delete-product/delete-product.component.ts
+++ frontend/src/app/components/partials/delete-product/delete-product.component.ts
@@ -1,7 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { FoodService } from 'src/app/services/food.service';
+import { FOODS_BY_ID_URL, FOODS_BY_TAG_URL } from 'src/app/shared/constants/urls';
+import { Food } from 'src/app/shared/models/Food';
 
 @Component({
   selector: 'delete-product',
@@ -10,11 +14,16 @@ import { FoodService } from 'src/app/services/food.service';
 })
 export class DeleteProductComponent implements OnInit{
 
-  deleteProduct!: FormGroup;
+  deleteProductForm!: FormGroup;
   isSubmitted = false;
   returnUrl = '';
 
+  categories = ['Sweets', 'Pastries', 'Our Cuisine'];
+  foods: Food[] = [];
+  products!: Food;
+
   constructor(
+    private http:HttpClient,
     private formBuilder: FormBuilder,
     private foodService: FoodService,
     private activatedRoute: ActivatedRoute,
@@ -22,7 +31,7 @@ export class DeleteProductComponent implements OnInit{
   ) {}
 
   ngOnInit(): void {
-    this.deleteProduct = this.formBuilder.group({
+    this.deleteProductForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       tags: ['', [Validators.required]],
     }
@@ -32,16 +41,30 @@ export class DeleteProductComponent implements OnInit{
 }
 
 get fc() {
-  return this.deleteProduct.controls;
+  return this.deleteProductForm.controls;
+}
+
+getFoodByTag(tag: string): Observable<Food[]>{
+  this.foodService.getFoodByTag(tag).subscribe(serverTags => {
+    this.foods = serverTags;
+  });
+  return this.http.get<Food[]>(FOODS_BY_TAG_URL + tag);
+}
+
+getFoodById(id: string): Observable<Food[]>{
+  this.foodService.getFoodById(id).subscribe(serverTags => {
+    this.products = serverTags;
+  });
+  return this.http.get<Food[]>(FOODS_BY_ID_URL + id);
 }
 
 submit() {
   this.isSubmitted = true;
-  if (this.deleteProduct.invalid)
+  if (this.deleteProductForm.invalid)
     return;
 
-  const fv = this.deleteProduct.value;
-  const product = fv.name;
+  const fv = this.deleteProductForm.value;
+  const product = fv.id;
 
   this.foodService.deleteProduct(product).subscribe((_) => {
     this.router.navigateByUrl(this.returnUrl);
